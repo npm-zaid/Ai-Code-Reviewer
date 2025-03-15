38165521f488bfe9a67df58c49d4415f9e6a5848
npm-zaid <starterapp000@gmail.com>
2025-03-15 17:39:58 +0530

    x

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,16 +5,10 @@ import cors from "cors";
 
 const app = express();
 
-// CORS Configuration
-const corsOptions = {
-  origin: "https://ai-code-reviewer-10.vercel.app", // Allow only this origin
-  methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
-  allowedHeaders: "Content-Type,Authorization", // Allowed headers
-};
 
-app.use(cors(corsOptions)); // Use the configured CORS options
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
